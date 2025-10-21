ac059216b909288c226c57e8a5924c5871d59648
RiteshSahany <SahanyRitesh@proton.me>
2025-10-21 14:24:28 +0530

    Revert "miatoll: Switch to NCM USB tethering"
    This reverts commit 7aee427579737fb80568a3c411eac2dde31ae0ab.

--- miatoll.mk
+++ miatoll.mk
@@ -202,8 +202,7 @@ PRODUCT_PACKAGES += \
     MiatollSettings \
     MiatollSystemUI \
     MiatollTelephony \
-    MiatollWifi \
-    NcmTetheringOverlay
+    MiatollWifi
 
 PRODUCT_PACKAGES += \
     SM6250Aperture
