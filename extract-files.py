fdec7a556522c7544994621ce5f85f3a3c827749
Ritesh <SahanyRitesh@proton.me>
2025-12-16 00:54:30 +0530

    fixup! miatoll: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM
    Change-Id: Ib47de2a5379e9786878875b67596bd44409938af

--- extract-files.py
+++ extract-files.py
@@ -105,10 +105,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libvendor.goodix.hardware.biometrics.fingerprint@2.1.so', 'vendor.goodix.hardware.biometrics.fingerprint@2.1.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    'system_ext/etc/init/wfdservice.rc': blob_fixup()
-        .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .remove_needed('android.hidl.base@1.0.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
